37829c9339464a283958f3d38fd61a64890bc88d
shinamon <76248816+shinamon610@users.noreply.github.com>
2024-01-01 20:47:55 +0900

    buildできるようにtauri由来のライブラリは動的importするようにした

--- src/models/file.ts
+++ src/models/file.ts
@@ -1,29 +1,26 @@
 import moment from "moment";
 import { Task } from "./task";
-import {
-  BaseDirectory,
-  writeTextFile,
-  readTextFile,
-  readDir,
-  createDir,
-} from "@tauri-apps/api/fs";
-import { save } from "@tauri-apps/api/dialog";
-import { appConfigDir } from "@tauri-apps/api/path";
 
 function selectFilePath(defaultPath: string): Promise<string | null> {
-  return save({
-    defaultPath,
-    filters: [{ name: "JSON", extensions: ["json"] }],
-  }).catch(() => null);
+  return import("@tauri-apps/api/dialog")
+    .then(({ save }) =>
+      save({
+        defaultPath,
+        filters: [{ name: "JSON", extensions: ["json"] }],
+      })
+    )
+    .catch(() => null);
 }
 
 async function ensureDirExists(dir: string) {
+  const { readDir, createDir } = await import("@tauri-apps/api/fs");
   try {
     await readDir(dir);
   } catch (error) {
     await createDir(dir, { recursive: true });
   }
 }
+
 const configFileName = "config.txt";
 
 async function saveFilePath(
@@ -34,8 +31,9 @@ async function saveFilePath(
     if (fp === null || fp === "") {
       return null;
     }
-    const targetDir = await appConfigDir();
-    await ensureDirExists(targetDir);
+    const { appConfigDir } = await import("@tauri-apps/api/path");
+    const { BaseDirectory, writeTextFile } = await import("@tauri-apps/api/fs");
+    await ensureDirExists(await appConfigDir());
     await writeTextFile(configFileName, fp, {
       dir: BaseDirectory.AppConfig,
     });
@@ -51,7 +49,8 @@ export function selectThenSaveFilePath(
   return saveFilePath(selectFilePath(defaultPath));
 }
 
-export function loadInitialFilePath(): Promise<string | null> {
+export async function loadInitialFilePath(): Promise<string | null> {
+  const { BaseDirectory, readTextFile } = await import("@tauri-apps/api/fs");
   return readTextFile(configFileName, {
     dir: BaseDirectory.AppConfig,
   });
@@ -61,12 +60,15 @@ type DataToSave = {
   tasks: Task[];
   userName: string;
 };
+
 export async function saveData(data: DataToSave, filePath: string) {
   const jsonContent = JSON.stringify(data);
+  const { writeTextFile } = await import("@tauri-apps/api/fs");
   await writeTextFile(filePath, jsonContent);
 }
 
 export async function loadData(filePath: string): Promise<DataToSave> {
+  const { readTextFile } = await import("@tauri-apps/api/fs");
   const jsonContent = await readTextFile(filePath);
   const { tasks, userName }: DataToSave = JSON.parse(jsonContent);
 
