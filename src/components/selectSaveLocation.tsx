453ec4b28aa74295a5a83a0f3e3f1e465f81ea79
shinamon <76248816+shinamon610@users.noreply.github.com>
2024-01-18 00:56:54 +0900

    buttonのui調整

--- src/components/selectSaveLocation.tsx
+++ src/components/selectSaveLocation.tsx
@@ -15,7 +15,12 @@ export function SelectSaveLocation({ setFilePath }: SelectSaveLocationProps) {
       }}
     >
       <button
-        style={{ backgroundColor: "var(--active)", fontSize: "10vh" }}
+        style={{
+          backgroundColor: "var(--active)",
+          fontSize: "10vh",
+          boxShadow: "5px 5px 10px rgba(255, 255, 255, 0.5)",
+          borderRadius: "10px",
+        }}
         onClick={() =>
           selectThenSaveFilePath().then((newFilePath) => {
             if (newFilePath === null) {
