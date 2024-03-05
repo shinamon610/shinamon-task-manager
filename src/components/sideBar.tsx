5897d892a61e46764f5ad8b206ddd39ce5cb83d3
shinamon <76248816+shinamon610@users.noreply.github.com>
2024-03-06 08:08:23 +0900

    依存関係のあるタスクを青で囲むようにした

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,7 +1,12 @@
 import { GlobalContext } from "@/contexts/globalContext";
 import { MainContext } from "@/contexts/mainContext";
 import { AccentColor, getSelectedStyle } from "@/lib/layoutUtils";
-import { Task, UUID } from "@/models/task";
+import {
+  Task,
+  UUID,
+  getAllTasksFromSource,
+  getAllTasksFromTarget,
+} from "@/models/task";
 import { Mode } from "@/vim/mode";
 import { useContext, useMemo } from "react";
 
@@ -11,19 +16,22 @@ type Card = {
   isSelected: boolean;
 };
 
-const CardComponent: React.FC<{ card: Card }> = ({ card }) => {
+const CardComponent: React.FC<{
+  card: Card;
+  style: {
+    padding: string;
+    border: string;
+  };
+}> = ({ card, style }) => {
   return (
-    <div
-      className="m-1 items-center bg-active"
-      style={getSelectedStyle(card.isSelected, AccentColor)}
-    >
+    <div className="m-1 items-center bg-active" style={style}>
       <div className="truncate">{card.title}</div>
     </div>
   );
 };
 
 export function SideBar() {
-  const { stackedTasks } = useContext(GlobalContext);
+  const { tasks, stackedTasks } = useContext(GlobalContext);
   const { mode } = useContext(MainContext);
   const cards: Card[] = useMemo(() => {
     return (stackedTasks.toJS() as Task[]).map((task) => ({
@@ -32,15 +40,30 @@ export function SideBar() {
       isSelected: task.isSelected,
     }));
   }, [stackedTasks]);
+  const dependentCards = useMemo(() => {
+    const selectedCard = cards.filter((card) => card.isSelected)[0];
+    if (selectedCard === undefined) return [];
+    const candidates = getAllTasksFromSource(tasks, selectedCard.id)
+      .concat(getAllTasksFromTarget(tasks, selectedCard.id))
+      .map(({ id }) => id);
+    return cards
+      .filter((card) => candidates.includes(card.id))
+      .map(({ id }) => id);
+  }, [cards, tasks]);
 
   return (
     <div
       className="bg-darkGray"
       style={getSelectedStyle(mode === Mode.SideBarSelecting, "blue")}
     >
-      {cards.map((card) => (
-        <CardComponent key={card.id} card={card} />
-      ))}
+      {cards.map((card) => {
+        const style = card.isSelected
+          ? getSelectedStyle(true, AccentColor)
+          : dependentCards.includes(card.id)
+            ? getSelectedStyle(true, "blue")
+            : getSelectedStyle(false, "");
+        return <CardComponent key={card.id} card={card} style={style} />;
+      })}
     </div>
   );
 }
