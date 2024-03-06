14a17c2e969bf5a5cd37459f03fc7b70b689daca
shinamon <76248816+shinamon610@users.noreply.github.com>
2024-03-07 02:36:50 +0900

    名前が被っていたのを修正

--- src/contexts/globalContext.tsx
+++ src/contexts/globalContext.tsx
@@ -116,13 +116,13 @@ export function GlobalProvider({ children }: { children: React.ReactNode }) {
     if (_tasks === undefined) {
       return List([]);
     }
-    const stackedTasks = _tasks
+    const _stackedTasks = _tasks
       .filter(
         (task) =>
           task.assignee === userName && task.status !== DefaultStatus.Done
       )
       .map(({ id }) => id);
-    return toposort(_tasks).filter((task) => stackedTasks.includes(task.id));
+    return toposort(_tasks).filter((task) => _stackedTasks.includes(task.id));
   }, [_tasks, userName]);
 
   const dependentIds = useMemo(() => {
